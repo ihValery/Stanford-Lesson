1a47a649ba2d0a9d626d09e291253487b6c7fde2
Valery <ihValery@gmail.com>
2021-03-22 01:18:46 +0300

    Finish lesson 1

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -2,16 +2,33 @@ import UIKit
 
 class ViewController: UIViewController
 {
-    var touches = 0
+    var touches = 0 {
+        didSet {
+            touchLabel.text = "Touches: \(touches)"
+        }
+    }
+    
+    let emojiCollection = ["🦄", "🦋", "🦄", "🦋"]
     
     @IBOutlet weak var touchLabel: UILabel!
     @IBOutlet var buttonCollection: [UIButton]!
     
     @IBAction func buttonAction(_ sender: UIButton)
     {
+        if let buttonIndex = buttonCollection.firstIndex(of: sender) {
+            flipButton(emoji: emojiCollection[buttonIndex], button: sender)
+        }
         touches += 1
-        touchLabel.text = "Touches: \(touches)"
     }
     
-    
+    func flipButton(emoji: String, button: UIButton)
+    {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0, green: 0.3231856227, blue: 0.5848054886, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
 }
